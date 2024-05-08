32d95962a3be956f8ad1b3776014d31d166715d8
dmingonette <diego_mingonette@hotmail.com>
2024-05-08 18:43:02 -0300

    Se iniciaron login_fragment.xml y LoginFragment.kt

---
+++ app/src/main/java/com/example/foodie/ui/fragments/LoginFragment.kt
@@ -0,0 +1,23 @@
+package com.example.foodie.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.foodie.databinding.LoginFragmentBinding
+
+
+class LoginFragment: Fragment() {
+
+    private lateinit var binding: LoginFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
